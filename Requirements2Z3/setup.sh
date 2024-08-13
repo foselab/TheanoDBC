0cbc9bc4fdab0fa49d5a583097aa1ae0959bbbc9
Marco Bisceglia <marco8bisceglia97@gmail.com>
2024-08-13 14:59:13 -0400

    Setup updated for macOS users

--- Requirements2Z3/setup.sh
+++ Requirements2Z3/setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-brew install gnu-sed
+sed_cmd="sed"
+
+os="$(uname -s)"
+
+if [[ "$os" == "Darwin" ]]; then
+	if ! command -v gsed > /dev/null 2>&1; then
+    brew install gnu-sed
+	fi
+	sed_cmd="gsed"
+fi
 
 mvn eclipse:clean
-
 mvn eclipse:eclipse
 
 # remove path from .classpath
-gsed -i '/src\/main\/java\/generated/d' ./.classpath
-
+$sed_cmd -i '/src\/main\/java\/generated/d' ./.classpath
