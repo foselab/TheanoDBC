b397f53badbc3e23982e0df8b035c6e1e362d7ea
Marco Bisceglia <marco8bisceglia97@gmail.com>
2024-09-28 13:53:16 -0400

    Improved code readability for ComposedRQTable class

--- Requirements2Z3/src/main/java/requirements2Z3/rqt/ComposedRQTable.java
+++ Requirements2Z3/src/main/java/requirements2Z3/rqt/ComposedRQTable.java
@@ -12,8 +12,8 @@ public class ComposedRQTable extends RQTable {
 	public ComposedRQTable() {
 		super(null, null, null); // init empty RQTable
 
-		this.contracts = new ArrayList<RQTable>();
-		this.chosenContracts = new ArrayList<RQTable>();
+		this.contracts = new ArrayList<>();
+		this.chosenContracts = new ArrayList<>();
 	}
 	
 	public ArrayList<RQTable> getContracts(){
@@ -60,29 +60,32 @@ public class ComposedRQTable extends RQTable {
         Scanner scanner = new Scanner(System.in);
         boolean done = false;
         
-    	while(!done && chosenContracts.size()<contracts.size()) {    
+    	while(!done) {    
 	        System.out.print("Please enter the name of the table to compose: ");
 	        
 	        String tableName = scanner.nextLine().trim();
 	        boolean found = this.chooseContract(tableName);
+	        System.out.println(found ? "Table added successfully": "Table not found");
 	        
-	        if (found)
-	        	System.out.println("Table added successfully");
-	        else
-	        	System.out.println("Table not found");  
+	        if(chosenContracts.size()>=contracts.size())
+	        	done=true;
 	        
-	        if(chosenContracts.size()>=2 && chosenContracts.size()<contracts.size()) {
-	        	boolean correctAnswer = false;
-	        	while(!correctAnswer) {
-	        		System.out.print("Do you want to choose other tables? [Y/N] ");
-		        	String answer = scanner.nextLine().trim().toLowerCase();
-		        	if (answer.equals("n")) {done=true;correctAnswer=true;}
-		        	else if (answer.toLowerCase().equals("y")) correctAnswer=true;
-		        	else System.out.println("Please choose between [Y/N] ");
-	        	}
+	        if(chosenContracts.size()>=2 && !done) {
+	        	done = askUserForMoreTables(scanner);
 	        }
         }
         scanner.close();
+	}
+	
+	// Ask the user to select tables for composition
+	private boolean askUserForMoreTables(Scanner scanner) {
+		while (true) {
+	        System.out.print("Do you want to choose other tables? [Y/N] ");
+	        String answer = scanner.nextLine().trim().toLowerCase();
+	        if (answer.equals("n")) return true;
+	        if (answer.equals("y")) return false;
+	        System.out.println("Please choose between [Y/N] ");
+	    }
 	}
 		
 	// if the name of the contract is available move the contract from contracts to chosenContract
@@ -90,7 +93,7 @@ public class ComposedRQTable extends RQTable {
 	private boolean chooseContract(String tableName) {
 		boolean found = false;
 		for (RQTable rt: this.contracts) {
-        	if (rt.getName().equals(tableName) & !this.chosenContracts.contains(rt)) {
+        	if (rt.getName().equals(tableName) && !this.chosenContracts.contains(rt)) {
         		this.chosenContracts.add(rt);
         		found = true;
         		break;
@@ -102,33 +105,30 @@ public class ComposedRQTable extends RQTable {
 	// compose the chosen contracts
 	private void composeContracts() throws Exception {		
 		if (this.chosenContracts.size()<2)
-			throw new Exception("composition command must be used at least on two tables");
+			throw new IllegalArgumentException("At least two tables must be selected for composition.");
 
 		// composition of the first two tables
 		RQTable first = this.chosenContracts.get(0);
 		RQTable second = this.chosenContracts.get(1);
 		
 		this.setVariables(getVariables(first, second));
-		this.setRequirements(getRequirements(first, second));
-		
-		if (chosenContracts.size()==2) return;
-		
+		this.setRequirements(composeRequirements(first, second));
+				
 		// compose every remaining table with the previous one
 		for(int i=2; i<chosenContracts.size(); i++) {
 			this.setVariables(getVariables(this, chosenContracts.get(i)));
-			this.setRequirements(getRequirements(this, chosenContracts.get(i)));
+			this.setRequirements(composeRequirements(this, chosenContracts.get(i)));
 		}
 	}
 	
 	// get all the variables from the two contracts
-	// TODO do not allow duplicates
 	private static Variables getVariables(RQTable first, RQTable second) {
 		Variables vars = first.getVariables();
-		vars.addAll(second.getVariables());
+		vars.addAll(second.getVariables()); // no duplicates since it is a set
 		return vars;
 	}
 	
-	private static Requirements getRequirements(RQTable first, RQTable second) {
+	private static Requirements composeRequirements(RQTable first, RQTable second) {
 		Requirement fst = first.getRequirements().getRequirement(0);
 		Requirement snd = second.getRequirements().getRequirement(0);
 		
@@ -137,8 +137,13 @@ public class ComposedRQTable extends RQTable {
 		PFormula A2 = snd.getPrecondition();
 		PFormula G2 = snd.getPostcondition(); 
 		
-		AndFormula post = new AndFormula(G1, G2);
-		OrFormula pre = new OrFormula(new AndFormula(A1, A2), new NegationFormula(post));
+		// postconditions
+		// G = G1 AND G2
+		AndFormula post = new AndFormula(G1, G2); 
+		
+		// preconditions
+		// A = (A1 AND A2) OR NOT (G1 AND G2)
+		OrFormula pre = new OrFormula(new AndFormula(A1, A2), new NegationFormula(post)); 
 		
 		Requirements req = new Requirements();
 		req.add(new Requirement(pre, post));
