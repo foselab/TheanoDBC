53910256742f89c007ab20334f154f2ce4ba3cc4
Marco Bisceglia <marco8bisceglia97@gmail.com>
2024-09-29 13:00:15 -0400

    Duplicate variables fixed

--- Requirements2Z3/src/main/java/requirements2Z3/rqt/Variable.java
+++ Requirements2Z3/src/main/java/requirements2Z3/rqt/Variable.java
@@ -1,5 +1,7 @@
 package requirements2Z3.rqt;
 
+import java.util.Objects;
+
 import requirements2Z3.visitors.RQTableVisitor;
 
 public class Variable {
@@ -27,6 +29,23 @@ public class Variable {
 	public String getInputOutput() {
 		return inputOutput;
 	}
+	
+	@Override
+    public boolean equals(Object obj) {
+		if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Variable other = (Variable) obj;
+
+        return Objects.equals(this.name, other.name) &&
+               Objects.equals(this.type, other.type) &&
+               Objects.equals(this.inputOutput, other.inputOutput);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.type, this.inputOutput);
+    }
 
 	public <T> T accept(RQTableVisitor<T> visitor) {
 		return visitor.visit(this);
