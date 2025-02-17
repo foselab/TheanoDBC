53421305227900e663a2cba49aa31247312d012f
Marco Bisceglia <marco8bisceglia97@gmail.com>
2025-02-17 16:41:38 +0100

    FSM script updated

--- Requirements2Z3/scripts/models/FSM/FSM.sh
+++ Requirements2Z3/scripts/models/FSM/FSM.sh
@@ -41,34 +41,43 @@ fi
 
 # Model Name
 model_name="FSM"
-current_version="FSM_v1" # FSM_v0 or FSM_v1
+system_name="FSM_system"
+versions=("FSM_v0" "FSM_v1") # FSM_v0 or FSM_v1
 
-# Files path
-model_file_path="$BASE_RESOURCES_PATH/${current_version}.rt"
-composition_file_path="$BASE_RESOURCES_PATH/${current_version}_composition.rt"
+for current_version in "${versions[@]}"; do    
+    refinement_file_name=${current_version}_refinement
 
-# Composition
-echo -e "\nRunning composition..."
-eval $cmd -i "$model_file_path" -o "$composition_file_path" -e BeUfFs -t composition -b 6 -a
+    # Files path
+    composition_file_path="$BASE_RESOURCES_PATH/${current_version}_composition.rt"
 
-# Refinement
-refinement_file_path="$BASE_RESOURCES_PATH/${current_version}_refinement.rt"
-composition_file_content=$(cat "$composition_file_path")
-system_file_content=$(cat "$BASE_RESOURCES_PATH/${model_name}_system.rt")
+    model_file_path="$BASE_RESOURCES_PATH/${current_version}.rt"
+    ota_file_path="$composition_file_path"
 
-echo "table System" > "$refinement_file_path"
-echo "$system_file_content" >> "$refinement_file_path"
-echo "endtable" >> "$refinement_file_path"
-echo "table Composition" >> "$refinement_file_path"
-echo "$composition_file_content" >> "$refinement_file_path"
-echo "endtable" >> "$refinement_file_path"
+    # Composition
+    echo -e "\nRunning composition..."
+    eval $cmd -i "$model_file_path" -o "$composition_file_path" -e BeUfFs -t composition -b 6 -a
 
-echo -e "\nRunning refinement..."
+    # Refinement
+    refinement_file_path="$BASE_RESOURCES_PATH/${refinement_file_name}.rt"
+    ota_file_content=$(cat "$ota_file_path")
+    system_file_content=$(cat "$BASE_RESOURCES_PATH/${system_name}.rt")
 
-refinement_script_path="$BASE_RESOURCES_PATH/${current_version}_refinement.py"
+    echo "table ${system_name}" > "$refinement_file_path"
+    echo "$system_file_content" >> "$refinement_file_path"
+    echo "endtable" >> "$refinement_file_path"
 
-eval $cmd -i "$refinement_file_path" -o "$refinement_script_path" -t refinement $redirect_output
-timeout 10 python "$refinement_script_path"
+    echo "table ${current_version}" >> "$refinement_file_path"
+    echo "$ota_file_content" >> "$refinement_file_path"
+    echo "endtable" >> "$refinement_file_path"
+
+    echo -e "\nDoes ${current_version} refine ${system_name}?"
+
+    refinement_script_path="$BASE_RESOURCES_PATH/${refinement_file_name}.py"
+
+    eval $cmd -i "$refinement_file_path" -o "$refinement_script_path" -t refinement $redirect_output
+    timeout 10 python "$refinement_script_path"
+
+done
 
 #cleanup_files
 echo -e "\nDone"
