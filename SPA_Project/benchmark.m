e22defa9ec469459a90ef27a835566f925d65862
Marco Bisceglia <marco8bisceglia97@gmail.com>
2026-04-29 11:03:06 +0200

    Added scripts to run SPA project experiments

---
+++ SPA_Project/benchmark.m
@@ -0,0 +1,125 @@
+function print_statistics(mean, median, std, min, max)
+    fprintf('\tAverage: %.2f\n', mean);
+    fprintf('\tMedian: %.2f\n', median);
+    fprintf('\tStandard Deviation: %.2f\n', std);
+    fprintf('\tMin: %.2f\n', min);
+    fprintf('\tMax: %.2f\n', max);
+end
+
+function print_boxplot(filename, d, labels, fig_title)
+    blue = [0, 114, 178]/255;
+    red = [230, 159, 0]/255;
+    gray = [187, 187, 187]/255;
+
+    figure;
+    h = boxplot(d);
+    hold on; % keep the boxplot
+
+    set(gca, 'FontSize', 14); % set font size
+    set(gcf, 'Name', fig_title); % figure name
+
+    means = mean(d);
+
+    % Add rhombus to the boxplot
+    for i = 1:length(means)
+        plot(i, means(i), 'd', 'MarkerSize', 10, 'Color', gray);
+    end
+    
+    grid on;
+    
+    % Thickness
+    set(findobj(gca, 'Type', 'Line'), 'LineWidth', 2);
+
+    % White background
+    set(gcf, 'Position', [200, 200, 350, 350], 'Color', 'w');
+    
+    % Labels
+    set(gca, 'XTickLabel', labels);
+    ylabel('Time [s]');
+    
+    % Get objects
+    boxes = findobj(h, 'Tag', 'Box');
+    medians = findobj(h, 'Tag', 'Median');
+    whiskers = findobj(h, 'Tag', 'Whisker');
+    caps = findobj(h, 'Tag', 'Cap');
+    outliers = findobj(h, 'Tag', 'Outliers');
+    
+    set(boxes, 'Color', blue, 'LineWidth', 2);
+    set(medians, 'Color', red, 'LineWidth', 2);
+    set(whiskers, 'Color', red, 'LineWidth', 2);
+    set(caps, 'Color', red, 'LineWidth', 2);
+    set(outliers, 'Color', red, 'LineWidth', 2);
+    set(outliers, 'MarkerEdgeColor', red, 'MarkerSize', 6); 
+    
+    print(filename,'-dpng','-r300');  % -dpng = PNG format, -r300 = 300 dpi
+    
+    hold off;
+end
+
+clear; clc; close all;
+
+% Read data from files
+T_safe = readtable('results/safe.csv');
+T_unsafe = readtable('results/unsafe.csv');
+
+T1 = T_safe(strcmp(T_safe.Solver, 'Theano_DC'), :);
+T2 = T_unsafe(strcmp(T_unsafe.Solver, 'Theano_DC'), :);
+T3 = T_safe(strcmp(T_safe.Solver, 'Theano_SC'), :);
+T4 = T_unsafe(strcmp(T_unsafe.Solver, 'Theano_SC'), :);
+T5 = T_safe(strcmp(T_safe.Solver, 'z3'), :);
+T6 = T_unsafe(strcmp(T_unsafe.Solver, 'z3'), :);
+T7 = T_safe(strcmp(T_safe.Solver, 'cvc5'), :);
+T8 = T_unsafe(strcmp(T_unsafe.Solver, 'cvc5'), :);
+
+idx_EUL_1 = startsWith(string(T1.OTA), "EUL"); M1_EUL = T1(idx_EUL_1, :); M1 = T1(~idx_EUL_1, :);
+idx_EUL_2 = startsWith(string(T2.OTA), "EUL"); M2_EUL = T2(idx_EUL_2, :); M2 = T2(~idx_EUL_2, :);
+idx_EUL_3 = startsWith(string(T3.OTA), "EUL"); M3_EUL = T3(idx_EUL_3, :); M3 = T3(~idx_EUL_3, :);
+idx_EUL_4 = startsWith(string(T4.OTA), "EUL"); M4_EUL = T4(idx_EUL_4, :); M4 = T4(~idx_EUL_4, :);
+idx_EUL_5 = startsWith(string(T5.OTA), "EUL"); M5_EUL = T5(idx_EUL_5, :); M5 = T5(~idx_EUL_5, :);
+idx_EUL_6 = startsWith(string(T6.OTA), "EUL"); M6_EUL = T6(idx_EUL_6, :); M6 = T6(~idx_EUL_6, :);
+idx_EUL_7 = startsWith(string(T7.OTA), "EUL"); M7_EUL = T7(idx_EUL_7, :); M7 = T7(~idx_EUL_7, :);
+idx_EUL_8 = startsWith(string(T8.OTA), "EUL"); M8_EUL = T8(idx_EUL_8, :); M8 = T8(~idx_EUL_8, :);
+
+time_column=3;
+
+E1 = M1{:, time_column}; E1_EUL = M1_EUL{:, time_column};
+E2 = M2{:, time_column}; E2_EUL = M2_EUL{:, time_column};
+E3 = M3{:, time_column}; E3_EUL = M3_EUL{:, time_column};
+E4 = M4{:, time_column}; E4_EUL = M4_EUL{:, time_column};
+E5 = M5{:, time_column}; E5_EUL = M5_EUL{:, time_column};
+E6 = M6{:, time_column}; E6_EUL = M6_EUL{:, time_column};
+E7 = M7{:, time_column}; E7_EUL = M7_EUL{:, time_column};
+E8 = M8{:, time_column}; E8_EUL = M8_EUL{:, time_column};
+
+% All data
+data = [E1, E2, E3, E4, E5, E6, E7, E8];
+data_eul = [E1_EUL, E2_EUL, E3_EUL, E4_EUL, E5_EUL, E6_EUL, E7_EUL, E8_EUL];
+
+% Statistics
+means = mean(data); means_eul = mean(data_eul);
+medians = median(data); medians_eul = median(data_eul);
+stds = std(data); stds_eul = std(data_eul);
+mins = min(data); mins_eul = min(data_eul);
+maxs = max(data); maxs_eul = max(data_eul);
+
+labels = {'Theano DC (s)', 'Theano DC (u)', 'Theano SC (s)', 'Theano SC (u)', 'z3 (s)', 'z3 (u)', 'cvc5 (s)', 'cvc5 (u)'};
+
+if ~all(isnan(means(:)))
+    fprintf('Statistics for each experiment (EUL excluded):\n');
+    for i = 1:length(means)
+        fprintf('%s\n', labels{i});
+        print_statistics(means(i), medians(i), stds(i), mins(i), maxs(i));
+    end
+
+    print_boxplot('results/boxplot', data, labels, 'Results for all the models (EUL excluded)');
+end
+
+if ~all(isnan(means_eul(:)))
+    fprintf('Statistics for each experiment (only EUL):\n');
+    for i = 1:length(means_eul)
+        fprintf('%s\n', labels{i});
+        print_statistics(means_eul(i), medians_eul(i), stds_eul(i), mins_eul(i), maxs_eul(i));
+    end
+
+    print_boxplot('results/boxplot_eul', data_eul, labels, 'Results for the EUL model');
+end
---
+++ SPA_Project/run.sh
@@ -0,0 +1,215 @@
+#!/usr/bin/env bash
+
+# Detect OS and set Python command
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OS" == "Windows_NT" ]]; then
+    python_cmd="py -3"
+    FIND_EXEC="find"
+else
+    python_cmd="python3"
+    FIND_EXEC="find"
+fi
+
+# Command
+cmd=(java -jar ../Theano.jar)
+
+# SETTINGS
+CLEAN_ON_EXIT=true
+ITERATIONS=50
+
+# PATHS TO SAVE RESULTS
+RESULT_PATH="./results"
+mkdir -p "$RESULT_PATH"
+SAFE_OTA_RESULTS="$RESULT_PATH/safe.csv"
+UNSAFE_OTA_RESULTS="$RESULT_PATH/unsafe.csv"
+
+# PATH TO ALL CONFIGURATIONS
+LMCHALLENGES_FOLDER="../LM_Challenges"
+
+# INITIAL CONFIGURATION FILES
+INIT_CONFIG=("TSM_v1" "FSM_v1" "TUI_v1" "REG_v1" "NLG_v1" "NN_v1" "EB_v1" "AP_v1" "SWIM_v1" "EUL_v1")
+UNSAFE_UPDATES=("TSM_v2" "FSM_v2" "TUI_v3" "REG_v2" "NLG_v2" "NN_v2" "EB_v2" "AP_v2" "SWIM_v2" "EUL_v2")
+SAFE_UPDATES=("TSM_v3" "FSM_v3" "TUI_v2" "REG_v3" "NLG_v3" "NN_v3" "EB_v3" "AP_v3" "SWIM_v3" "EUL_v3")
+#INIT_CONFIG=("EUL_v1")
+#UNSAFE_UPDATES=("EUL_v2")
+#SAFE_UPDATES=("EUL_v3")
+
+# CLEANUP FUNCTION
+cleanup_files() {
+	find "$LMCHALLENGES_FOLDER" -type f \( -name "*.py" -o -name "*.bak" -o -name "*_refinement.rt" -o -name "*_composition.rt" -o -name "*.smt" \) -delete
+}
+
+get_result() {
+	local input_str="$1"
+	if [[ "$input_str" == *"unsat"* ]]; then
+		echo "safe"
+	elif [[ "$input_str" == *"sat"* ]]; then
+		echo "unsafe"
+	else
+		echo "unknown"
+	fi
+}
+
+# Run refinement, it takes as input the name of the OTA and the file to save the results
+run_refinement() {
+	local ota=$1
+	local refinement_time_file=$2
+
+	# Model paths
+	model_name="${ota/_v[0-9]/}" # e.g. TSM
+	model_path="${LMCHALLENGES_FOLDER}/${model_name}" # e.g. TSM
+	
+	# Current system and OTA paths
+	system_name="${model_name}_v1" # e.g TSM_v1
+	system_name_path="${model_path}/${system_name}.rt" # e.g. TSM_v1.rt
+	ota_file_path="${model_path}/${ota}.rt" # e.g. TSM_v3.rt
+	refinement_file_path="${model_path}/${ota}_refinement.rt" # e.g. TSM_v3_refinement.rt
+
+	# Paths for generated scripts
+	refinement_script_path_dc="${model_path}/${ota}_refinement_dc.py" # e.g. TSM_v3_refinement.py
+	refinement_script_path_sc="${model_path}/${ota}_refinement_sc.py" # e.g. TSM_v3_refinement.py
+	ota_file_smtlib_path="${model_path}/${ota}.smt" # e.g. TSM_v3.smt
+	smtlib_script_path="${model_path}/${ota}_smtlib.py" # e.g. TSM_v3_smtlib.py
+
+	# Check if required files exist
+	if [[ ! -f "$system_name_path" || ! -f "$ota_file_path" ]]; then
+		echo "Missing required file(s) for $ota, skipping..." >> "$refinement_time_file"
+		return
+	fi
+
+	# Get tables 
+	system_file_content=$(cat "$system_name_path")
+	ota_file_content=$(cat "$ota_file_path")
+	
+	# Create refinement file
+	{
+		echo "table ${system_name}"
+		echo "$system_file_content"
+		echo "endtable"
+		echo "table ${ota}"
+		echo "$ota_file_content"
+		echo "endtable"
+	} > "$refinement_file_path"
+
+	# Check if refinement file exists
+	if [ -f "$refinement_file_path" ]; then
+		for ((i = 1; i <= ITERATIONS; i++)); do
+			
+			echo "Running $ota, iteration $i..."
+
+			# if the OTA is FSM then we need to run composition first
+			if [[ "$ota" == *"FSM"* ]]; then
+				composition_file_path="${model_path}/${ota}_composition.rt" # e.g. FSM_v1_composition.rt
+				
+				# Create composition file
+				if ! "${cmd[@]}" -i "$ota_file_path" -o "$composition_file_path" -t composition -e BeUfFs -t composition -b 6 -a > /dev/null 2>&1; then
+					echo "$ota,$i,CompositionFailed" >> "$refinement_time_file"
+					continue
+				fi
+
+				# Create refinement file with composition
+				{
+					echo "table ${system_name}"
+					cat "$system_name_path"
+					echo
+					echo "endtable"
+					echo "table ${ota}"
+					cat "$composition_file_path"
+					echo
+					echo "endtable"
+				} > "$refinement_file_path"
+			fi
+
+			# Generate refinement script with double check
+			if ! "${cmd[@]}" -i "$refinement_file_path" -o "$refinement_script_path_dc" -t refinement > /dev/null 2>&1; then
+				echo "$ota,$i,JavaFailed,Theano_DC" >> "$refinement_time_file"
+				continue
+			fi
+
+			# Generate refinement script with single check
+			if ! "${cmd[@]}" -i "$refinement_file_path" -o "$refinement_script_path_sc" -t refinement -s > /dev/null 2>&1; then
+				echo "$ota,$i,JavaFailed,Theano_SC" >> "$refinement_time_file"
+				continue
+			fi
+
+			# Generate SMT-LIB file (single check)
+			"${cmd[@]}" -i "$refinement_file_path" -o "$smtlib_script_path" -t smtlib > /dev/null 2>&1
+			$python_cmd "$smtlib_script_path" > "$ota_file_smtlib_path"
+
+			# Measure time for refinement with double check
+			start_time=$(date +%s.%N)
+
+			# Run refinement check with double check
+			theano_result_dc="$($python_cmd "$refinement_script_path_dc")"
+			if [ $? -ne 0 ]; then
+				echo "$ota,$i,PythonFailed,Theano_DC" >> "$refinement_time_file"
+				continue
+			fi
+			
+			end_time=$(date +%s.%N)
+			elapsed=$(python -c "print($end_time-$start_time)")
+
+			echo -e "\tTheano (Double Check): $theano_result_dc" 
+			echo "$ota,$i,$elapsed,$theano_result_dc,Theano_DC" >> "$refinement_time_file"
+
+			# Measure time for refinement with single check
+			start_time=$(date +%s.%N)
+
+			# Run refinement check with single check
+			theano_result_sc="$($python_cmd "$refinement_script_path_sc")"
+			if [ $? -ne 0 ]; then
+				echo "$ota,$i,PythonFailed,Theano_SC" >> "$refinement_time_file"
+				continue
+			fi
+			
+			end_time=$(date +%s.%N)
+			elapsed=$(python -c "print($end_time-$start_time)")
+
+			echo -e "\tTheano (Single Check): $theano_result_sc" 
+			echo "$ota,$i,$elapsed,$theano_result_sc,Theano_SC" >> "$refinement_time_file"	
+
+			# Measure time for refinement check with SMT-LIB file with Z3
+			start_time=$(date +%s.%N)
+			z3_res_str=$(z3 $ota_file_smtlib_path -T:10 | head -n 1)
+			end_time=$(date +%s.%N)
+			z3_res=$(get_result "$z3_res_str")
+			elapsed=$(python -c "print($end_time-$start_time)")
+			echo -e "\tz3: $z3_res"
+			echo "$ota,$i,$elapsed,$z3_res,z3" >> "$refinement_time_file"
+
+			# Measure time for refinement check with SMT-LIB file with cvc5
+			start_time=$(date +%s.%N)
+			cvc5_res_str=$(cvc5 $ota_file_smtlib_path --tlimit-per=10000 | head -n 1)
+			end_time=$(date +%s.%N)
+			cvc5_res=$(get_result "$cvc5_res_str")
+			elapsed=$(python -c "print($end_time-$start_time)")
+			echo -e "\tcvc5: $cvc5_res"
+			echo "$ota,$i,$elapsed,$cvc5_res,cvc5" >> "$refinement_time_file"
+		done
+	else
+		echo "FAIL TO CREATE FILE for $ota" >> "$refinement_time_file"
+	fi
+}
+
+trap 'echo -e "\nCtrl+C detected. Exiting..."; [ "$CLEAN_ON_EXIT" = true ] && cleanup_files; exit 0' SIGINT
+
+echo "OTA,Iteration,Time,Result,Solver" > "$SAFE_OTA_RESULTS"
+echo "OTA,Iteration,Time,Result,Solver" > "$UNSAFE_OTA_RESULTS"
+
+echo -e "Running OTA...\n"
+
+# RUN REFINEMENT FOR SAFE UPDATES
+for ota in "${SAFE_UPDATES[@]}"; do
+	run_refinement "$ota" "$SAFE_OTA_RESULTS"
+done
+
+# RUN REFINEMENT FOR UNSAFE UPDATES
+for ota in "${UNSAFE_UPDATES[@]}"; do
+	run_refinement "$ota" "$UNSAFE_OTA_RESULTS"
+done
+
+# CLEANUP FILES
+if [ "$CLEAN_ON_EXIT" = true ]; then
+	cleanup_files
+fi
+
+echo -e "OTA times results saved to $SAFE_OTA_RESULTS, $UNSAFE_OTA_RESULTS\n"
